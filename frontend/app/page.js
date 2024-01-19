696d4ede8af15fbd38d7642b4f79e90668ff5d5b
Prabhjot Singh <pjsingh1359@gmail.com>
2024-01-19 15:31:49 -0800

    add sign in and sign up routes

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,9 +1,11 @@
-import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <Link href="/pages">CLick Here</Link>
+      <div className="routes">
+        <a href="/signIn"><button>Sign In</button></a>
+        <a href="/signUp"><button>Sign Up</button></a>
+      </div>
     </main>
   );
 }
