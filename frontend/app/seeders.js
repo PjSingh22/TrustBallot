ca5ecdc1921e86d543a679cea6bd93d94568d30a
koreanpro22 <dhskim22@gmail.com>
2024-01-19 18:59:37 -0500

    add seeders

---
+++ frontend/app/seeders.js
@@ -0,0 +1,10 @@
+[{"id":1,"first_name":"Cathe","last_name":"Chater","email":"cchater0@nih.gov","password":"fF44@ROu\\g?8","address":"62916 Mockingbird Avenue","state":"CA","ssn":"435-47-8710"},
+{"id":2,"first_name":"Glynda","last_name":"Dobeson","email":"gdobeson1@ezinearticles.com","password":"nS5HRnQeHiAj1","address":"41435 Marcy Circle","state":"CA","ssn":"109-38-5161"},
+{"id":3,"first_name":"Hertha","last_name":"Brine","email":"hbrine2@prnewswire.com","password":"iC1`6\\!>","address":"0547 Ryan Road","state":"CA","ssn":"173-14-9775"},
+{"id":4,"first_name":"Maddy","last_name":"Guidini","email":"mguidini3@tripod.com","password":"kR277\"{mF|)","address":"887 Forest Run Pass","state":"CA","ssn":"797-66-2003"},
+{"id":5,"first_name":"Sheela","last_name":"McAlroy","email":"smcalroy4@amazon.co.uk","password":"gN6Ju3G_Xhp","address":"197 American Parkway","state":"CA","ssn":"634-31-9250"},
+{"id":6,"first_name":"Claybourne","last_name":"Hayworth","email":"chayworth5@youku.com","password":"uJ3Kg|4kaFb","address":"28975 Westport Parkway","state":"CA","ssn":"295-36-3242"},
+{"id":7,"first_name":"Edithe","last_name":"Sillick","email":"esillick6@mtv.com","password":"pD3LIW@D_Ptt","address":"4140 Carioca Center","state":"CA","ssn":"389-05-8722"},
+{"id":8,"first_name":"Kata","last_name":"Lalevee","email":"klalevee7@reddit.com","password":"fS6p?Tc/Mh,'C","address":"3587 Mayer Trail","state":"CA","ssn":"794-93-5564"},
+{"id":9,"first_name":"Antoinette","last_name":"Jackman","email":"ajackman8@networkadvertising.org","password":"eN8ZO=s@,|Ro","address":"5516 Knutson Plaza","state":"CA","ssn":"204-37-4950"},
+{"id":10,"first_name":"Scarlet","last_name":"Melsome","email":"smelsome9@blinklist.com","password":"bA0OFbI`#","address":"3 Moland Court","state":"CA","ssn":"741-73-6823"}]
