3a2400706103c97e3c1fafd8d86340d835285c1a
Prabhjot Singh <pjsingh1359@gmail.com>
2024-01-19 16:33:25 -0800

    saving work

--- frontend/app/signIn/page.js
+++ frontend/app/signIn/page.js
@@ -1,15 +1,20 @@
 "use client"
 import React, { useState } from 'react'
+import seeders from '../seeders'
 
 export default function signIn() {
   const [id, setId] = useState('')
   const [password, setPassword] = useState('')
   const [ssn, setSsn] = useState('')
 
+  console.log(id)
+
   const handleSubmit = (e) => {
     e.preventDefault()
 
   }
+
+
 
   return (
     <section>
@@ -17,15 +22,15 @@ export default function signIn() {
       <form onSubmit={handleSubmit}>
         <label htmlFor='id'>
           ID Number/Driver ID
-          <input id='id' type="text" placeholder='Type info here...' />
+          <input id='id' type="text" placeholder='Type info here...' onChange={(e) => setId(e.target.value)} />
         </label>
         <label htmlFor='password'>
           Password
-          <input id='password' type="password" placeholder='Type info here...' />
+          <input id='password' type="password" placeholder='Type info here...' onChange={(e) => setPassword(e.target.value)} />
         </label>
         <label htmlFor='ssn'>
           SSN
-          <input id='ssn' type="text" placeholder='Type info here...' />
+          <input id='ssn' type="text" placeholder='Type info here...' onChange={(e) => setSsn(e.target.value)} />
         </label>
         <button type='submit'>Sign In</button>
       </form>
