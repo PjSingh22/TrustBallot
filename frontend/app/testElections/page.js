fd41a1b9f490d1e2ca21b60ffda28ed421920627
koreanpro22 <dhskim22@gmail.com>
2024-01-20 15:36:06 -0500

    starting new test elections

---
+++ frontend/app/testElections/page.js
@@ -0,0 +1,14 @@
+"use client"
+import React, { useState } from "react";
+import { Radio, RadioGroup, Stack } from '@chakra-ui/react'
+import './testElections.css';
+
+export default function testElections() {
+
+    return (
+        <>
+            Testing
+        </>
+    )
+
+}
