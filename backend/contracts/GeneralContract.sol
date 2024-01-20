a19ca8da4d6c75298196df41c269a341b940d03c
Prabhjot Singh <pjsingh1359@gmail.com>
2024-01-20 10:46:53 -0800

    finished creating smart contracts

--- backend/contracts/GeneralContract.sol
+++ backend/contracts/GeneralContract.sol
@@ -6,6 +6,7 @@ contract GeneralContract {
 
   struct Vote {
     uint id;
+    uint voterId;
     uint year;
     uint congressionalCandidateId;
     uint stateCandidateId;
@@ -17,4 +18,36 @@ contract GeneralContract {
 
   Vote[] public votes;
   mapping(uint256 => address) public voteToOwner;
+
+  function addVote(
+    uint _voterId,
+    uint _year,
+    uint _congressionalCandidateId,
+    uint _stateCandidateId,
+    uint _upperStateLegislativeCandidateId,
+    string memory _congressionalCandidate,
+    string memory _stateCandidate,
+    string memory _upperStateLegislativeCandidate
+  ) external {
+    uint id = votes.length;
+    votes.push(Vote(id, _voterId, _year, _congressionalCandidateId, _stateCandidateId, _upperStateLegislativeCandidateId, _congressionalCandidate, _stateCandidate, _upperStateLegislativeCandidate));
+    voteToOwner[id] = msg.sender;
+    emit AddVote(msg.sender, id);
+  }
+
+  function getMyVotes() external view returns (Vote[] memory) {
+    Vote[] memory temporary = new Vote[](votes.length);
+    uint counter = 0;
+    for (uint i = 0; i < votes.length; i++) {
+      if (voteToOwner[i] == msg.sender) {
+        temporary[counter] = votes[i];
+        counter++;
+      }
+    }
+    Vote[] memory myVotes = new Vote[](counter);
+    for (uint i = 0; i < counter; i++) {
+      myVotes[i] = temporary[i];
+    }
+    return myVotes;
+  }
 }
--- backend/contracts/PrimaryContract.sol
+++ backend/contracts/PrimaryContract.sol
@@ -6,6 +6,7 @@ contract PrimaryContract {
 
   struct Vote {
     uint id;
+    uint voterId;
     uint congressionalCandidateId;
     uint stateCandidateId;
     uint upperStateLegislativeCandidateId;
@@ -14,5 +15,41 @@ contract PrimaryContract {
     string stateCandidate;
     string upperStateLegislativeCandidate;
     string lowerStateLegislativeCandidate;
+  }
+
+  Vote[] public votes;
+  mapping(uint256 => address) public voteToOwner;
+
+  function addVote(
+    uint _voterId,
+    uint _congressionalCandidateId,
+    uint _stateCandidateId,
+    uint _upperStateLegislativeCandidateId,
+    uint _lowerStateLegislativeCandidateId,
+    string memory _congressionalCandidate,
+    string memory _stateCandidate,
+    string memory _upperStateLegislativeCandidate,
+    string memory _lowerStateLegislativeCandidate
+  ) external {
+    uint id = votes.length;
+    votes.push(Vote(id, _voterId, _congressionalCandidateId, _stateCandidateId, _upperStateLegislativeCandidateId, _lowerStateLegislativeCandidateId, _congressionalCandidate, _stateCandidate, _upperStateLegislativeCandidate, _lowerStateLegislativeCandidate));
+    voteToOwner[id] = msg.sender;
+    emit AddVote(msg.sender, id);
+  }
+
+  function getMyVotes() external view returns (Vote[] memory) {
+    Vote[] memory temporary = new Vote[](votes.length);
+    uint counter = 0;
+    for (uint i = 0; i < votes.length; i++) {
+      if (voteToOwner[i] == msg.sender) {
+        temporary[counter] = votes[i];
+        counter++;
+      }
+    }
+    Vote[] memory myVotes = new Vote[](counter);
+    for (uint i = 0; i < counter; i++) {
+      myVotes[i] = temporary[i];
+    }
+    return myVotes;
   }
 }
