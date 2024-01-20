9cf51783feac993aaf196eb89bf53a533a18cbe9
koreanpro22 <dhskim22@gmail.com>
2024-01-19 21:40:39 -0500

    resolve conflict

--- frontend/app/signUp/page.js
+++ frontend/app/signUp/page.js
@@ -7,14 +7,22 @@ import {
   Button,
   Select,
   Option,
+  Text,
   FormErrorMessage,
   FormHelperText,
 } from '@chakra-ui/react'
+import './signup.css';
 
 export default function signUp() {
-  const [id, setId] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [state, setState] = useState('')
+  const [address, setAddress] = useState('')
   const [ssn, setSsn] = useState('')
+  const [id, setId] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -22,44 +30,42 @@ export default function signUp() {
   }
 
   return (
-    <section>
-      <h1>Sign Up</h1>
-
-      <FormControl isRequired>
+    <section className='sign-up-form'>
+      <Text fontSize='4xl' color="gold">Sign Up</Text>
+      <form>
+      <FormControl>
         <FormLabel>First name
-          <Input placeholder='First name' />
-        </FormLabel>
-
+          <Input required id='' type='' placeholder='First name' onChange={(e) => setFirstName(e.target.value)} />
+        </FormLabel>        
         <FormLabel>Last name
-          <Input placeholder='Last name' />
-        </FormLabel>
-
+          <Input required id='' type='' placeholder='Last name' onChange={(e) => setLastName(e.target.value)} />
+        </FormLabel>        
         <FormLabel>Email address
-          <Input placeholder='Email'/>
-        </FormLabel>
-
+          <Input required id='' type='' placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
+        </FormLabel>        
         <FormLabel>Password
-          <Input placeholder='Password'/>
-        </FormLabel>
-
+          <Input required id='' type='' placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
+        </FormLabel>        
         <FormLabel>Confirm Password
-          <Input placeholder='Confirm Password'/>
-        </FormLabel>
-
-        <Select placeholder='State'>
+          <Input required id='' type='' placeholder='Confirm Password' onChange={(e) => setConfirmPassword(e.target.value)} />
+        </FormLabel>       
+        <FormLabel>Primary Address
+          <Input required id='' type='' placeholder='Primary Address' onChange={(e) => setAddress(e.target.value)}/>
+        </FormLabel>        
+        <FormLabel>State
+        <Select required id='' type='' placeholder='--' onChange={(e) => setState(e.target.value)} >
           <option>CA</option>
-        </Select>
-
+        </Select>        
+        </FormLabel>
         <FormLabel>SSN
-          <Input placeholder='SSN'/>
-        </FormLabel>
-
+          <Input required id='' type='' placeholder='SSN' onChange={(e) => setSsn(e.target.value)} />
+        </FormLabel>        
         <FormLabel>ID Number/Driver ID
-          <Input placeholder='ID'/>
-        </FormLabel>
-
-        <Button type='submit'>Submit</Button>
+          <Input required id='' type='' placeholder='State ID'/>
+        </FormLabel>        
       </FormControl>
+      <button type='submit'>Sign Up</button>
+      </form>
     </section>
   )
 }
