eebfe0ee2877340e9a8fe0cd02dad6b1455dabf8
Prabhjot Singh <pjsingh1359@gmail.com>
2024-01-20 21:11:59 -0800

    add styles to dashboard

--- frontend/app/electionsDashboard/page.js
+++ frontend/app/electionsDashboard/page.js
@@ -4,6 +4,7 @@ import { useUserContext } from '../../context';
 import primaryElectionAbi from '../../../backend/build/contracts/PrimaryContract.json'
 import { PrimaryContractAddress } from "../../config"
 import { ethers } from "ethers";
+import './styles.css'
 
 export default function electionsDashboard() {
   const { votes, setVotes } = useUserContext();
@@ -31,26 +32,27 @@ export default function electionsDashboard() {
 
 
   return (
-    <div>
+    <div className='dashboard'>
       <h1>Elections Dashboard</h1>
       <div className="previous-elections">
         No Previous Elections
       </div>
-      {votes.length ? 
-        (<>
+      {votes.length ?
+        (<p className='already-voted'>
           You have already voted for the current election
-        </>) :
+        </p>) :
         (<div className="current-elections">
-        <a href='/testElections'>Vote for current elections</a>
+        <a href='/testElections'>Vote for current election</a>
       </div>)}
       <div className="future-elections">
         Currently These Elections are not open for voting
       </div>
+      <p className='previous-votes'>Previous votes</p>
       <div className='votes'>
         {votes.map((vote) => {
           vote = Object.values(vote)
           return (
-            <div>
+            <div className='vote'>
               <h1>Vote for current election</h1>
               <p>State Candidate: {vote[6]}</p>
               <p>Upper State Legislative Candidate: {vote[7]}</p>
